18701cd870d802c6827ef41527ac6918e7859190
nikolai-churakov <cni.zip@gmail.com>
2022-06-07 23:24:48 +0300

    ++

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world22!</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            <h1>add to git</h1>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
