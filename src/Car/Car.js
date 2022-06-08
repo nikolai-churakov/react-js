2d388adcdf300254d8eab9fce5b71db1d6fc8e40
nikolai-churakov <cni.zip@gmail.com>
2022-06-08 23:06:34 +0300

    add car.js component

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -22,9 +22,7 @@ import React from "react";
 export default () => (
     <div>
        <p>This is car component</p>
-        <p>
-            <strong style={{color:'red'}}>
-                {Math.round(Math.random()*100)}</strong>
+        <p>Number: <strong style={{color:'red'}}>{Math.round(Math.random()*100)}</strong>
         </p>
     </div>
 )
