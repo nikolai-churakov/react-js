c9255a300d517530b131a1d68c4f12e549be31e9
nikolai-churakov <cni.zip@gmail.com>
2022-06-10 22:35:04 +0300

    add car.js component

--- src/App.js
+++ src/App.js
@@ -11,12 +11,9 @@ function App() {
     <div className="App">
         <div style={divStyle}>
             <h1 style={{fontSize: '30px', textAlign: 'center'}}>add to git</h1>
-            <Car name={'Ford'} year={2018}>
-                <p style={{color: 'green'}}>COLOR</p>
-            </Car>
-            <Car name={'Audi'} year={2015}>
-               <p style={{color: 'red'}}>COLOR</p>
-            </Car>
+
+            <Car name={'Ford'} year={2018}/>
+            <Car name={'Audi'} year={2015}/>
             <Car name={'Mazda'} year={2022}/>
         </div>
     </div>
