b030fb75de748bce6688c7897cddc71b8f87e634
nikolai-churakov <cni.zip@gmail.com>
2022-06-10 22:58:53 +0300

    add car.js component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import Car from "./Car/Car";
-import {Component} from "react";
-
-state = {
-   cars: [
-       {name: 'Ford', year: 2018},
-       {name: 'Audi', year: 2016},
-       {name: 'Mazda', year: 2010}
-]};
-
-const cars = this.state.cars
+import React, { Component } from 'react';
+//
+// state = {
+//    cars: [
+//        {name: 'Ford', year: 2018},
+//        {name: 'Audi', year: 2016},
+//        {name: 'Mazda', year: 2010}
+// ]};
 
 class App extends Component {
     render() {
@@ -20,11 +18,11 @@ class App extends Component {
         return (
             <div className="App">
                 <div style={divStyle}>
-                    <h1 style={{fontSize: '30px', textAlign: 'center'}}>add to git</h1>
+                    <h1 style={{fontSize: '30px', textAlign: 'center'}}>add to git react</h1>
 
-                    <Car name={cars[0].name} year={cars[0].year}/>
-                    <Car name={cars[1].name} year={cars[1].year}/>
-                    <Car name={cars[2].name} year={cars[2].year}/>
+                    {/*<Car name={cars[0].name} year={cars[0].year}/>*/}
+                    {/*<Car name={cars[1].name} year={cars[1].year}/>*/}
+                    {/*<Car name={cars[2].name} year={cars[2].year}/>*/}
                 </div>
             </div>
         );
