65662e1b1344be0903de9775317a7af23dceafd3
nikolai-churakov <cni.zip@gmail.com>
2022-06-12 22:11:57 +0300

    add car.js component

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ const divStyle =  {
 
 function App() {
 
-    this.state = {
-        cars: [
-            {name: 'Ford', year: 2018},
-            {name: 'Audi', year: 2016},
-            {name: 'Mazda', year: 2010}
-        ]
-    }
-    const cars = this.state.cars
+    // this.state = {
+    //     cars: [
+    //         {name: 'Ford', year: 2018},
+    //         {name: 'Audi', year: 2016},
+    //         {name: 'Mazda', year: 2010}
+    //     ]
+    // }
+    // const cars = this.state.cars
     const [count, setCount] = useState(0);
 
     return (
@@ -29,8 +29,8 @@ function App() {
             <h1 style={{fontSize: '30px', textAlign: 'center'}}>add to git</h1>
 
             <Car name={'cars[0].name'} year={'cars[0].year'}/>
-            <Car name={cars[1].name} year={cars[1].year}/>
-            <Car name={cars[2].name} year={cars[2].year}/>
+            {/*<Car name={cars[1].name} year={cars[1].year}/>*/}
+            {/*<Car name={cars[2].name} year={cars[2].year}/>*/}
         </div>
         <div>
             <p>Вы кликнули {count +1} раз(а)</p>
