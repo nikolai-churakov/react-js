cc358ce74fdb497deb75725030d9a215e82d4fd2
nikolai-churakov <cni.zip@gmail.com>
2022-06-14 13:51:19 +0300

    add car.js component

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -24,6 +24,6 @@ export default props => (
        <h3>Car name: {props.name}</h3>
         <p>Year: {props.year}</p>
         <p>Lot number: <strong style={{color:'red'}}>{Math.round(Math.random()*100)}</strong></p>
-        <button>Click</button>
+        <button onClick={onChangeTitle}>Click</button>
     </div>
 )
