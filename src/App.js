e5ec6688b6bf568f8e046ddb236115b202a28479
nikolai-churakov <cni.zip@gmail.com>
2022-06-14 13:02:26 +0300

    add car.js component

--- src/App.js
+++ src/App.js
@@ -14,18 +14,21 @@ class App extends Component {
             {name: 'Ford', year: 2018},
             {name: 'Audi', year: 2016},
             {name: 'Mazda', year: 2010}
-        ]
+        ],
+        pageTitle: 'React components'
     }
+
 
 render() {
     const cars = this.state.cars
 
-
-
     return (
     <div className="App">
         <div style={divStyle}>
-            <h1 style={{fontSize: '30px', textAlign: 'center'}}>add to git</h1>
+            <h1 style={{fontSize: '30px', textAlign: 'center'}}>{this.state.pageTitle}</h1>
+
+            <button onClick={}>Change title</button>
+
             <Car name={cars[0].name} year={cars[0].year}/>
             <Car name={cars[1].name} year={cars[1].year}/>
             <Car name={cars[2].name} year={cars[2].year}/>
