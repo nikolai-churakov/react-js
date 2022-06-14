4156c9162190f45ddceb188f5984b35b2c0ac7c8
nikolai-churakov <cni.zip@gmail.com>
2022-06-14 13:45:58 +0300

    add car.js component

--- src/App.js
+++ src/App.js
@@ -38,9 +38,13 @@ render() {
 
             <button onClick={this.changeTitleHandler}>Change title</button>
 
-            <Car name={cars[0].name} year={cars[0].year}/>
-            <Car name={cars[1].name} year={cars[1].year}/>
-            <Car name={cars[2].name} year={cars[2].year}/>
+            <Car name={cars[0].name}
+                 year={cars[0].year}/>
+                onChangeTitle={this.changeTitleHandler}
+            <Car name={cars[1].name}
+                 year={cars[1].year}/>
+            <Car name={cars[2].name}
+                 year={cars[2].year}/>
         </div>
     </div>
   );
