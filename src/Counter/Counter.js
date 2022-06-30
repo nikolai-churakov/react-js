d1e84d98c93f9358c1323bb72423dd3e3ace21fb
nikolai-churakov <cni.zip@gmail.com>
2022-06-30 22:03:25 +0300

    add car.js component

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -25,7 +25,6 @@ export default class Counter extends Component {
                 <button onClick={() => this.setState({counter: this.state.counter -1})}>-</button>
             </Auxiliary>
         )
-
         // return [
         //         <h2 key={'1'}>Counter {this.state.counter}</h2>,
         //         <button key={'2'} onClick={this.addCounter}>+</button>,
